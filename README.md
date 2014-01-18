b108f4c6be207340460268a4044320fcda1ee2b1
Lukas Zeller <luz@plan44.ch>
2014-01-18 17:53:13 +0100

    Updated to Pebble SDK 2.0

--- README.md
+++ README.md
@@ -14,6 +14,11 @@ I'd be happy about contributions of other Swiss German dialect time texts (or in
 
 **Important**: This code is based on Pebble's early beta or even alpha proof-of-concept SDK, so it is highly probable this code will break and need adaption in the near future as Pebble SDK matures. **Use entirely on you own risk**.
 
-To install and use the pebble SDK, please follow the instructions at [developer.getpebble.com](http://developer.getpebble.com/1/welcome).
+To install and use the pebble SDK, please follow the instructions at [developer.getpebble.com/2](https://developer.getpebble.com/2/).
 
 To download the compiled watchface (.pbw file) or the original iOS app, please see the [Ziit page here](http://www.mixwerk.ch/ziit_app.php)
+
+Version History:
+----------------
+1.1.0 : April 2013: initial version for beta Pebble SDK 1.0
+2.1.0 : January 2014: updated for Pebble SDK 2.0
--- src/ziit.c
+++ src/ziit.c
@@ -1,21 +1,11 @@
 /*
 
   Ziit watch
-  (c) 2010-2013 by Lukas Zeller + Frank Hürlimann, mixwerk.ch, plan44.ch
+  (c) 2010-2014 by Lukas Zeller + Frank Hürlimann, mixwerk.ch, plan44.ch
 
  */
 
-#include "pebble_os.h"
-#include "pebble_app.h"
-#include "pebble_fonts.h"
-
-
-#define MY_UUID { 0xFF, 0xA3, 0x15, 0x94, 0x1D, 0x30, 0x44, 0x99, 0xBF, 0xA1, 0x00, 0x74, 0x48, 0xF1, 0xE5, 0xCD }
-PBL_APP_INFO(MY_UUID,
-             "Ziit", "plan44.ch",
-             1, 1, /* App version */
-             RESOURCE_ID_IMAGE_MENU_ICON,
-             APP_INFO_WATCH_FACE);
+#include <pebble.h>
 
 // hour texts
 #define LONGEST_HOUR_TEXT 6 // for debugging layout
@@ -102,10 +92,10 @@ const char * const minuteTexts[60] = {
 
 // Variables
 // - the window
-Window window;
+Window *window;
 // - layers
-Layer second_display_layer;
-TextLayer ziit_display_textlayer;
+Layer *second_display_layer;
+TextLayer *ziit_display_textlayer;
 // - internal variables
 int currentSecond;
 int textValid;
@@ -120,11 +110,12 @@ char ziit_str_buffer[ZIIT_STR_BUFFER_BYTES];
 #define DOT_RADIUS 4
 
 
-void second_display_layer_callback(Layer *me, GContext* ctx)
+void second_display_layer_callback(struct Layer *layer, GContext *ctx)
 {
   // unsigned int angle = currentSecond * 6;
   int32_t hexAngle = (int32_t)currentSecond * 1092;
-  GPoint center = grect_center_point(&me->frame);
+  GRect f = layer_get_frame(layer);
+  GPoint center = grect_center_point(&f);
   int32_t r = center.x-DOT_MARGIN;
   GPoint dotPos;
   dotPos.x = center.x + sin_lookup(hexAngle)*r/0xFFFFl;
@@ -140,85 +131,92 @@ void second_display_layer_callback(Layer *me, GContext* ctx)
 #define DEBUG_SECONDS_AS_MIN 0
 
 // handle second tick
-void handle_second_tick(AppContextRef ctx, PebbleTickEvent *event)
+void handle_second_tick(struct tm *tick_time, TimeUnits units_changed)
 {
-  currentSecond = event->tick_time->tm_sec;
+  currentSecond = tick_time->tm_sec;
   #if DEBUG_SECONDS_AS_MIN
   textValid = 0; // update text every second
   #else
-  if (currentSecond==0) textValid = 0; // new minute, needs new text
+  if (units_changed & MINUTE_UNIT) textValid = 0; // new minute, needs new text
   #endif
   if (!textValid) {
     #if DEBUG_SECONDS_AS_MIN
     int16_t minTextIndex = currentSecond;
     int16_t hourTextIndex = LONGEST_HOUR_TEXT; // longest text
     #else
-    int16_t minTextIndex = event->tick_time->tm_min;
-    int16_t hourTextIndex = (event->tick_time->tm_hour + (minTextIndex>=NEXT_HOUR_REF_MIN ? 1 : 0)) % 12;
+    int16_t minTextIndex = tick_time->tm_min;
+    int16_t hourTextIndex = (tick_time->tm_hour + (minTextIndex>=NEXT_HOUR_REF_MIN ? 1 : 0)) % 12;
     #endif
     strcpy(ziit_str_buffer, "es isch\n");
     strcat(ziit_str_buffer, minuteTexts[minTextIndex]); // includes needed line break (usually at end, sometimes in the middle)
     strcat(ziit_str_buffer, hourTexts[hourTextIndex]);
-    text_layer_set_text(&ziit_display_textlayer, ziit_str_buffer);
+    text_layer_set_text(ziit_display_textlayer, ziit_str_buffer);
     textValid = 1; // now valid
   }
   // anyway, update second
-  layer_mark_dirty(&second_display_layer);
+  layer_mark_dirty(second_display_layer);
 }
 
 
 
-void handle_init(AppContextRef ctx)
+void init()
 {
-  (void)ctx;
-
   // the window
-  window_init(&window, "Ziit");
-  window_set_background_color(&window, GColorBlack);
-  window_stack_push(&window, true /* Animated */);
+  window = window_create();
+  window_set_background_color(window, GColorBlack);
+  window_stack_push(window, true /* Animated */);
 
   // init the engine
   textValid = 0; // text not valid
-  PblTm t;
-  get_time(&t);
-  currentSecond = t.tm_sec; // start with correct second position
+  struct tm *t;
+  time_t now = time(NULL);
+  t = localtime(&now);
+  currentSecond = t->tm_sec; // start with correct second position
 
+  // get the window frame
+  GRect wf = layer_get_frame(window_get_root_layer(window));
   // the text layer for displaying the Ziit time text
-  GRect tf = window.layer.frame;
+  GRect tf = wf; // start calculation with window frame
   // - centered square
   tf.origin.x += TEXT_LAYER_MINMARGIN;
   tf.size.w -= 2*TEXT_LAYER_MINMARGIN;
   tf.origin.y += (tf.size.h-tf.size.w) / 2 + TEXT_LAYER_EXTRAMARGIN_H;
   tf.size.h = tf.size.w - 2*TEXT_LAYER_EXTRAMARGIN_H;
-  text_layer_init(&ziit_display_textlayer, tf);
+  ziit_display_textlayer = text_layer_create(tf);
   // - parameters
-  text_layer_set_text_alignment(&ziit_display_textlayer, GTextAlignmentCenter); // centered
-  text_layer_set_background_color(&ziit_display_textlayer, GColorBlack); // black background
-  text_layer_set_font(&ziit_display_textlayer, fonts_get_system_font(FONT_KEY_GOTHIC_28)); // font
-  text_layer_set_text_color(&ziit_display_textlayer, GColorWhite); // white text
+  text_layer_set_text_alignment(ziit_display_textlayer, GTextAlignmentCenter); // centered
+  text_layer_set_background_color(ziit_display_textlayer, GColorBlack); // black background
+  text_layer_set_font(ziit_display_textlayer, fonts_get_system_font(FONT_KEY_GOTHIC_28)); // font
+  text_layer_set_text_color(ziit_display_textlayer, GColorWhite); // white text
   // - text
   strcpy(ziit_str_buffer, "Ziit\n©2013\nplan44.ch");
-  text_layer_set_text(&ziit_display_textlayer, ziit_str_buffer);
-  layer_add_child(&window.layer, &ziit_display_textlayer.layer);
+  text_layer_set_text(ziit_display_textlayer, ziit_str_buffer);
+  layer_add_child(window_get_root_layer(window), (Layer *)ziit_display_textlayer);
 
   // the layer for displaying the seconds dot
-  layer_init(&second_display_layer, window.layer.frame);
-  second_display_layer.update_proc = &second_display_layer_callback;
-  layer_add_child(&window.layer, &second_display_layer);
-  layer_mark_dirty(&second_display_layer); // draw immediately
+  second_display_layer = layer_create(wf);
+  layer_set_update_proc(second_display_layer, &second_display_layer_callback);
+  layer_add_child(window_get_root_layer(window), second_display_layer);
+  layer_mark_dirty(second_display_layer); // draw immediately
 
+  // now subscribe to ticks
+  tick_timer_service_subscribe(SECOND_UNIT, &handle_second_tick);
+}
+
+
+void deinit()
+{
+  layer_destroy(second_display_layer);
+  text_layer_destroy(ziit_display_textlayer);
+  window_destroy(window);
 }
 
 
 
-void pbl_main(void *params)
+int main(void)
 {
-  PebbleAppHandlers handlers = {
-    .init_handler = &handle_init,
-    .tick_info = {
-      .tick_handler = &handle_second_tick,
-      .tick_units = SECOND_UNIT
-    }
-  };
-  app_event_loop(params, &handlers);
+  init();
+  app_event_loop();
+  deinit();
+  return 0;
 }
